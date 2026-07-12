4f0fec68a797854023f0394dcf174939843b8ddb
santusht06 <santusht.kotai@startsappss.com>
2026-07-12 16:22:58 +0530

    feat: configure dynamic backend port listener using PORT environment variable

--- backend/main.py
+++ backend/main.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8001,
+        port=int(os.getenv("PORT", 8001)),
         reload=True,
         log_level="info",
     )
